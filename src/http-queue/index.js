04edb8ee13a73e471ef26cdc52d2535017827395
fenkman <45321259+fenkman@users.noreply.github.com>
2019-11-01 07:23:23 +0100

    Update index.js
    ... for slack events

--- src/http-queue/index.js
+++ src/http-queue/index.js
@@ -2,10 +2,11 @@ module.exports = function (context, req) {
 
     let model = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
     let error = !model ? "no data; or invalid payload in body" : null;
+    let challenge = model ? model.challenge : null;
 
     context.res = {
         status: error ? 500 : 200,
-        body: error
+        body: error ? error : challenge
     };
 
     context.bindings.out = model;
